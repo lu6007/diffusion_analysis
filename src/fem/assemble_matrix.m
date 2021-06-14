7409e97d4a86b7b5aafc20fee4cd4672d26be138
lu6007 <shaoying.lu@gmail.com>
2021-06-14 15:48:12 -0700

    Improved comments.

--- src/fem/assemble_matrix.m
+++ src/fem/assemble_matrix.m
@@ -1,13 +1,17 @@
 % function [K,M] = assemble_matrix(p,tri)
 % assemble the stiffness matrix K and the mass matrix M
+% function [K,M] = assemble_matrix(p,tri,varargin)
+% parameter_name = {'diff_coef'};
+% default_value = {1};
+% diff_coef can be a constant or a row vector at the triangle centers of
+% mass, but it cannot be a column vector. 
 
 % Copyright: Shaoying Lu and Yingxiao Wang 2012
 function [K,M] = assemble_matrix(p,tri,varargin)
 parameter_name = {'diff_coef'};
 default_value = {1};
 diff_coef = parse_parameter(parameter_name, default_value, varargin);
-% diff_coef can be a constant or a row vector at the triangle centers of
-% mass.
+
 
 c = diff_coef; a = 1; f=0;
 % c the diffusion coefficient, which can be a row vector of the same
